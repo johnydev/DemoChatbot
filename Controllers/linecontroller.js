3663a4363b73aa766fd8ef2ce7d11af5e30cb4ba
narongchai.k <narongchai.k@softsq.com>
2018-03-29 17:31:20 +0700

    sss

--- Controllers/linecontroller.js
+++ Controllers/linecontroller.js
@@ -76,7 +76,7 @@ function handleEvent(event) {
                     // error handling
                 });
                 //if (event.message.text == 'map') {
-                    echo = { type: 'location', 
+                    const echox = { type: 'location', 
                     title: 'Soft SQ Co.,Ltd.',
                     address: 'Central Tower1 Building',
                     latitude: 13.668498,
@@ -86,7 +86,7 @@ function handleEvent(event) {
                // else {
                //     const echo = { type: 'text', text: event.message.text };
                // }
-             lineclient.pushMessage(event.source.userId,echo);
+             lineclient.pushMessage(event.source.userId,echox);
         });
 
 
